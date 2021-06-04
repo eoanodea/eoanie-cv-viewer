05e7b1fe8142ad4ddc1e53f928811231c3e96dd2
Eoan O Dea <odeaeoan@gmail.com>
2021-06-04 10:44:04 +0200

    Updated Github workflow with GA secret

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -40,7 +40,6 @@ const Header = ({ history }: any) => {
   const [isEnglish, setIsEnglish] = React.useState(true);
   const [displaySwitch, setDisplaySwitch] = React.useState(false);
 
-  console.log("Initializing GA", config.ga_id);
   ReactGA.initialize(config.ga_id);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
